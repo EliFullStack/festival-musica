1fe7d54b83003df1cd4a9b6fd59e1dda0f956e4e
unknown <elisabet.v.alvarez@gmail.com>
2023-04-06 12:22:38 +0200

    adding Plumber plugin

---
+++ README.md
@@ -0,0 +1,14 @@
+***
+npm i
+***
+***
+npm run sass 
+***
+
+***
+npm run css 
+***
+
+***
+npx gulp dev -nombre de la tarea-  ó npm run dev aunque para ello debo ponerlo en scripts
+***
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const { src, dest, watch } = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 //compile style sheet
 function css(callback) {
 
     src('src/scss/**/*.scss') //identify SASS file
+        .pipe(plumber())
         .pipe( sass() ) //compile it
         .pipe(dest('build/css')); //store it on hard drive
     
