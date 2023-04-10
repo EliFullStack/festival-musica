2940780b3e66530db56f11a10a7c4c1cc7005ce2
unknown <elisabet.v.alvarez@gmail.com>
2023-04-10 17:24:09 +0200

    using gulp to create webp and avif images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Images
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 //compile style sheet
 function css(callback) {
@@ -13,6 +21,36 @@ function css(callback) {
     callback();
 }
 
+function images(callback) {
+    const options = {
+        optimizationLevel: 3
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(options)))
+        .pipe(dest('build/img'))
+    callback();
+}
+
+function webpVersion(callback) {
+    const options = {
+        quality:50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(options))
+        .pipe(dest('build/img'))
+    callback();
+}
+
+function avifVersion(callback) {
+    const options = {
+        quality:50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(options))
+        .pipe(dest('build/img'))
+    callback();
+}
+
 function dev(callback) {
 
     watch('src/scss/**/*.scss', css);
@@ -21,4 +59,7 @@ function dev(callback) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.images = images;
+exports.webpVersion = webpVersion;
+exports.avifVersion = avifVersion;
+exports.dev = parallel(images, webpVersion, avifVersion, dev);
